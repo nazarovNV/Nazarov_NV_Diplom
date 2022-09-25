8872b3f5062d31339d5df634fc9599c737b091a6
Nikolay Nazarov <nikolaynazarovvv216@gmail.com>
2022-09-25 12:33:16 +0300

    2

---
+++ tests/scripts_collector.py
@@ -0,0 +1,15 @@
+import allure
+import pytest
+
+
+# <-------------------------------=1-1=------------------------------->
+
+@pytest.fixture()
+def select_address_0(go_to_main_page, element, driver):
+    go_to_main_page
+
+
+@pytest.fixture()
+def select_address_1(select_address_0, element_chose_address, element, driver):
+    select_address_0
+    element_chose_address.click()
